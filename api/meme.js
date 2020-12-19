6b79821bee614db78afed92086e6a6b43d2c35f5
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2020-12-19 11:11:25 +0530

    Update meme.js

--- api/meme.js
+++ api/meme.js
@@ -1,161 +1,158 @@
 const data = [
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_33_06_j8fdmb.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_33_25_gvkb3w.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_33_32_ackq6j.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_33_35_eifjpi.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_33_41_zykdox.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_33_45_hw88si.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_33_51_iqgfmw.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_34_01_vag2pb.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_34_13_ql68pz.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_34_23_d0x6fc.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_34_27_udrygn.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_34_32_mcuiyc.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_34_36_phwupo.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_34_41_gsyrkt.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_34_45_jfnph4.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_34_48_xypfvj.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_34_52_pd2aik.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_34_55_vcidzv.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_02_vqig4k.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_05_xpccap.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_10_jnjjep.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_14_f2rar1.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_17_wpffvt.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_22_caoqn0.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_24_a2eznx.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_26_iqvolk.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_32_ha26dd.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_36_d5qcn7.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_46_ys8nuz.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_49_tcvmrs.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_52_qfxtr4.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_35_54_sjevg5.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_36_02_xhxwpk.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_36_06_jpiche.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_36_11_i81uc7.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_36_15_r87yhn.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_36_20_xozchz.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_36_24_bo9jrz.jpg",
     },
     {
-      “img”:
+      "img":
         "https://raw.githubusercontent.com/alenwelch/image-/main/memes/IMAGE_2020-12-19_03_36_29_u4ldww.jpg",
     },
   ];
-  
-  
-  
